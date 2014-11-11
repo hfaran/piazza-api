c3c84237df23dc29bf606c637822e1fcd9c1efeb
hfaran <hamzafaran@hotmail.com>
2014-11-10 22:54:17 -0800

    feat(user): Add feed methods

--- piazza_api/network.py
+++ piazza_api/network.py
@@ -1,6 +1,46 @@
 from .rpc import PiazzaRPC
 
 
+################
+# Feed Filters #
+################
+
+class FeedFilter(object):
+    pass
+
+class UnreadFilter(FeedFilter):
+    """Filter through only posts with unread content in feed"""
+    def __init__(self):
+        pass
+
+    def to_kwargs(self):
+        return dict(updated=True)
+
+class FollowingFilter(FeedFilter):
+    """Filter through only posts that you are following in feed"""
+    def __init__(self):
+        pass
+
+    def to_kwargs(self):
+        return dict(following=True)
+
+class FolderFilter(FeedFilter):
+    """Filter only posts in ``folder_name`` in your feed
+
+    :type folder_name: str
+    :param folder_name: Name of folder to show posts from in feed
+    """
+    def __init__(self, folder_name):
+        self.folder_name = folder_name
+
+    def to_kwargs(self):
+        return dict(folder=True, filter_folder=self.folder_name)
+
+
+###########
+# Network #
+###########
+
 class Network(object):
     """Abstraction for a Piazza "Network" (or class)
 
@@ -24,7 +64,7 @@ class Network(object):
         :rtype: dict
         :returns: Dictionary with all data on the post
         """
-        return self._rpc.content_get(cid)
+        return self._rpc.content_get(cid=cid)
 
     def iter_all_posts(self):
         """
@@ -50,7 +90,7 @@ class Network(object):
             of dicts containing user data.
         :rtype: list
         """
-        return self._rpc.get_users(user_ids)
+        return self._rpc.get_users(user_ids=user_ids)
 
     def iter_users(self, user_ids):
         """Same as ``Network.get_users``, but returns an iterable instead
@@ -89,7 +129,7 @@ class Network(object):
             of dicts of user data of all of the users in the network
             including the ones that were just added.
         """
-        return self._rpc.add_students(student_emails)
+        return self._rpc.add_students(student_emails=student_emails)
 
     def remove_users(self, user_ids):
         """Remove users with ``user_ids`` from this network
@@ -102,8 +142,62 @@ class Network(object):
             of dicts of user data of all of the users remaining in
             the network after users are removed.
         """
-        return self._rpc.remove_users(user_ids)
+        return self._rpc.remove_users(user_ids=user_ids)
 
     ########
     # Feed #
     ########
+
+    def get_feed(self, limit=100, offset=0):
+        """Get your feed for this network
+
+        Pagination for this can be achieved by using the ``limit`` and
+        ``offset`` params
+
+        :type limit: int
+        :param limit: Number of posts from feed to get, starting from ``offset``
+        :type offset: int
+        :param offset: Offset starting from bottom of feed
+        :type sort: str
+        :rtype: list
+        :returns: List of posts in feed format; this means they are not the
+            full posts but only in partial form as necessary to display them
+            on the Piazza feed. For example, the returned dicts only
+            have content snippets of posts rather than the full text.
+        """
+        return self._rpc.get_my_feed(limit=limit, offset=offset)
+
+    def get_filtered_feed(self, feed_filter):
+        """Get your feed containing only posts filtered by ``feed_filter``
+
+        :type feed_filter: FeedFilter
+        :param feed_filter: Must be an instance of either: UnreadFilter,
+            FollowingFilter, or FolderFilter
+        :rtype: list
+        """
+        assert isinstance(feed_filter, (UnreadFilter, FollowingFilter,
+                                        FolderFilter))
+        return self._rpc.filter_feed(**feed_filter.to_kwargs())
+
+    def search_feed(self, query):
+        """Search for posts with ``query``, returned in feed format
+
+        :type query: str
+        :param query: The search query; should just be keywords for posts
+            that you are looking for
+        :rtype: list
+        """
+        return self._rpc.search(query=query)
+
+    ##############
+    # Statistics #
+    ##############
+
+    def get_statistics(self):
+        """Get statistics for class
+
+        :rtype: dict
+        :returns: Statistics for class that are viewable on the Statistics
+            page on the Piazza web UI
+        """
+        return self._rpc.get_stats()
